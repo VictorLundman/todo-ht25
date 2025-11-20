62ec2e481dce1f62372a1db8665f52b451bc4542
VictorLundman <186269292+VictorLundman@users.noreply.github.com>
2025-11-20 16:35:46 +0100

    Enkel CRUD

--- app.rb
+++ app.rb
@@ -4,12 +4,89 @@ require 'slim'
 require 'sinatra/reloader'
 
 
+def connectToDb()
+    db = SQLite3::Database.new("db/todos.db")
+    db.results_as_hash = true
 
+    return db
+end
 
+def getTodoById(db, id)
+    todo = db.execute("SELECT * FROM todos WHERE id=?", id)
+    if !todo
+        return nil
+    end
 
-# Routen /
+    return todo[0]
+end
+
 get '/' do
     slim(:index)
 end
 
+get "/todos" do
+    db = connectToDb()
+    
+    @todos = db.execute("SELECT * FROM todos")
 
+    slim(:"todos/index")
+end
+
+post "/todos" do
+    db = connectToDb()
+
+    name = params[:name]
+    description = params[:description]
+
+    if name.include? "roblox" or name.include? "robux"
+        return redirect("https://tenor.com/p8jv5bc6AED.gif")
+    end
+
+    db.execute("INSERT INTO todos (name, description) VALUES (?, ?)", [name, description])
+
+    redirect("/todos")
+end
+
+get "/todos/:id/edit" do
+    id = params[:id].to_i
+    db = connectToDb()
+
+    @todo = getTodoById(db, id)
+    if !@todo
+        error(404)
+    end
+
+    slim(:"todos/edit")
+end
+
+post "/todos/:id/update" do
+    id = params[:id].to_i
+    db = connectToDb()
+
+    @todo = getTodoById(db, id)
+    p @todo
+    if !@todo
+        error(404)
+    end
+
+    name = params[:name]
+    description = params[:description]
+
+    db.execute("UPDATE todos SET name=?, description=? WHERE id=?", [name, description, id])
+
+    redirect("/todos")
+end
+
+post "/todos/:id/delete" do
+    id = params[:id].to_i
+    db = connectToDb()
+
+    @todo = getTodoById(db, id)
+    if !@todo
+        error(404)
+    end
+
+    db.execute("DELETE FROM todos WHERE id=?", id)
+
+    redirect("/todos")
+end
